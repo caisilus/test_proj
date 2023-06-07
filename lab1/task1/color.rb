cb2da2ed14a56dd7ec40cc6eaa7db15a6ff15adb
caisilus <serangor@gmail.com>
2023-06-07 23:55:49 +0300

    0

--- lab1/task1/color.rb
+++ lab1/task1/color.rb
@@ -1,18 +1,23 @@
 def color(r, g, b)
+  pass_0(r, g, b)
   # pass_33(r, g, b)
-  pass_66(r, g, b)
+  # pass_66(r, g, b)
   # solve(r, g, b)
 end
 
-def pass_33(r,g,b)
+def pass_0(r, g, b)
+  nil
+end
+
+def pass_33(r, g, b)
   [r,g,b].map { |component| component_to_hex(component) }.join
 end
 
-def pass_66(r,g,b)
+def pass_66(r, g, b)
   [r,g,b].map { |component| component_to_hex(component) }.join.upcase
 end
 
-def solve(r,g,b)
+def solve(r, g, b)
   [r,g,b].map {|component| component_to_hex(component.clamp(0, 255)) }.join.upcase # passes 100%
 end
 
