640bd72edbf4ad37134bfefa3360561b1e1cf76d
caisilus <serangor@gmail.com>
2023-06-07 23:44:21 +0300

    Try solve 2

--- lab1/task1/color.rb
+++ lab1/task1/color.rb
@@ -1,4 +1,8 @@
 def color(r, g, b)
-  # "FFFFFF"
-  r.to_s(16) + g.to_s(16) + b.to_s(16) # passes
+  # "FFFFFF" # passes 33%
+  [r,g,b].map {|component| component_to_hex(component) }.join.upcase # passes 100%
+end
+
+def component_to_hex(component)
+  component.to_s(16).rjust(2, '0')
 end
